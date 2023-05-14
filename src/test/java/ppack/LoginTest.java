8e39eadd32b14aab734fddeae60587d6778af99d
QAJani <krupa0602@gmail.com>
2023-05-14 17:24:22 -0400

    Main method setup

--- src/test/java/ppack/LoginTest.java
+++ src/test/java/ppack/LoginTest.java
@@ -1,10 +1,26 @@
 package ppack;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import objectrepository.LoginPage;
 
 public class LoginTest {
     @Test
     public void login() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://tutorialsninja.com/demo/index.php?route=account/login");
 
+        LoginPage loginpage = new LoginPage();
+        loginpage.emailField().sendKeys("Tester@test.com");
+        loginpage.passwordField().sendKeys("null123");
+        loginpage.loginButton().click();
     }
 }
